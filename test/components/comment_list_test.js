e09888779580015366ef194eb44e82872bf3abae
ekingan <ekingan@gmail.com>
2016-08-27 20:19:44 -0700

    testing example

---
+++ test/components/comment_list_test.js
@@ -0,0 +1,22 @@
+import { renderComponent, expect } from '../test_helper';
+import CommentList from '../../src/components/comment_list';
+
+describe('CommentList', () => {
+
+	let component;
+
+	beforeEach(() => {
+		const props = { comments: ['a comment', 'another comment'] };
+		component = renderComponent(CommentList, null, props);
+	});
+
+	it('shows an LI for each comment', () => {
+		expect(component.find('li').length).to.equal(2);
+	});
+
+	it('shows each comment that is provided', () => {
+		expect(component).to.contain('a comment');
+		expect(component).to.contain('another comment');
+	});
+
+});
